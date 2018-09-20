86894464e7c1966aa324b7de9b253a3aad4a186f
Kavindu Damsara <kavindudiu@gmail.com>
2018-09-20 12:33:14 +0530

    structuring

--- app/src/main/java/com/example/user/heartratemonitor/DeviceListActivity.java
+++ app/src/main/java/com/example/user/heartratemonitor/DeviceListActivity.java
@@ -89,7 +89,7 @@ public class DeviceListActivity extends Activity {
             String address = info.substring(info.length() - 17);
 
             // Make an intent to start next activity while taking an extra which is the MAC address.
-            Intent i = new Intent(DeviceListActivity.this, BlueTooth.class);
+            Intent i = new Intent(DeviceListActivity.this, NavActivity.class);
             i.putExtra(EXTRA_DEVICE_ADDRESS, address);
             startActivity(i);
         }
